f1a3658155a2f530b106f679254900397c6df829
Ruth <ruro122020@gmail.com>
2023-09-16 11:23:33 -0400

    render dog names to DOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', init)
 //global variables
-let dogsArray;
+let dogsArray = []
+
+//helper functions
+function loopThroughDogs(){
+    dogsArray.forEach(dog => renderDog(dog))
+}
+//render to DOM
+function renderDog({id, name, isGoodDog, image}){
+    //get parent element
+    const dogBarContainer = document.getElementById('dog-bar')
+    //create element
+    const dogName = document.createElement('span')
+    //add text
+    dogName.textContent = name
+    //append to DOM
+    dogBarContainer.appendChild(dogName)
+}
 //fetch requests
 function getDogs(){
     fetch('http://localhost:3000/pups')
     .then(res => res.json())
-    .then(dogs => dogsArray = dogs)
+    .then(dogs => {
+        dogsArray = dogs
+        loopThroughDogs()
+    })
 }
 
 function init(){
